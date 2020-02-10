ec051faea7c7bb146cd023d555c2570e21586ede
Sam <bell137@gmail.com>
2020-02-10 08:00:17 -0600

    Update server.py

--- app/server.py
+++ app/server.py
@@ -7,9 +7,11 @@ from io import BytesIO
 
 from fastai.vision import *
 
-model_file_url = 'https://www.dropbox.com/s/y4kl2gv1akv7y4i/stage-2.pth?raw=1'
+#model_file_url = 'https://www.dropbox.com/s/y4kl2gv1akv7y4i/stage-2.pth?raw=1'
+model_file_url = 'https://www.dropbox.com/s/tlocg5c37ljless/stage-2.pth?raw=1'
 model_file_name = 'model'
-classes = ['black', 'grizzly', 'teddys']
+#classes = ['black', 'grizzly', 'teddys']
+classes= ['truck','car']
 path = Path(__file__).parent
 
 app = Starlette()
